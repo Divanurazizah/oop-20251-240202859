9784543e8b68529f33ec882b3f3d8360d07c3dba
Divanurazizah <nurazizahjebor12@gmail.com>
2025-10-03 11:36:16 +0700

    Create HelloProcedural.java

---
+++ praktikum/week1-setup-hello-pos/src/HelloProcedural.java
@@ -0,0 +1,8 @@
+public class HelloProcedural {
+    public static void main(String[] args) {
+        // Paradigma prosedural: langsung instruksi urut
+        String nama = "Diva Nur Azizah";
+        String nim = "240202859";
+        System.out.println("Hello World, I am " + nama + "-" + nim);
+    }
+}
